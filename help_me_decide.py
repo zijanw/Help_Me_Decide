e7bb4f171525f5cf2a7081ccd1b3c1909c8d458c
Zi-Jan W <zijanwong@gmail.com>
2022-07-22 14:22:02 -0500

    some clean-up

--- help_me_decide.py
+++ help_me_decide.py
@@ -1,30 +1,39 @@
 import PySimpleGUI as sg    # to open a new window
 import os.path              # python's os module
 import random               # random number generator
+
+
 ############################### class
 class Main_menu:
+
+    # main
     def main_main_menu():
         run = 1
+        # run forever until user quits
         while run == 1:
             Main_menu.print_menu()
             selection = Main_menu.get_menu_selection()
             Main_menu.exec_menu(selection)
 
+    # print main menu
     def print_menu():
         print("Yo! Welcome to Help-Me-Decide!\n" + 
             "\nYour options are: \n" + 
             " 1. Computer, decide for me please!\n" +
-            " 2. Wait can I add more to the pool?\n" + 
-            " 3. Let's see what do we have here...\n" + 
+            " 2. Wait can I add more to the list?\n" + 
+            " 3. Help me recall what I had...\n" + 
             " 4. Okay, I'm done. Can I leave now?\n")
     
+    # get input from user
     def get_menu_selection():
         return input("Enter your selection: ")
     
+    # quit program
     def exit_program():
         print("\nSee you next time!\n")
         exit()
 
+    # execute methods based on user input
     def exec_menu(selection):
         if selection == "1":
             Main_menu.get_random_from_saved()
@@ -37,10 +46,10 @@ class Main_menu:
         else:
             print("Invalid selection. Please try again.\n")
         
+    # randomize from pool
     def get_random_from_saved():
         saved = ["Thai Kitchen", "Taste Place"]
         print(random.choice(saved))
-
 
 ############################### main menu
 Main_menu.main_main_menu()
